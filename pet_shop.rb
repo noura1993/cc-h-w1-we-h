70b5a9b8b2d3f3ed01aba0b7c26075bec780315d
Nourhan Elhakim <nourhanmagdy993@gmail.com>
2020-05-16 22:44:31 +0100

    Find if customer can afford pet

--- pet_shop.rb
+++ pet_shop.rb
@@ -65,3 +65,7 @@ end
 def add_pet_to_customer(customer, new_pet)
     customer[:pets].push(new_pet)
 end
+
+def customer_can_afford_pet(customer, new_pet)
+    return customer[:cash] >= new_pet[:price]
+end
