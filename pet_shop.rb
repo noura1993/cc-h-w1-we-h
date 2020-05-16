fee253c84a88dd06ad8f1d032c66343eee762e25
Nourhan Elhakim <nourhanmagdy993@gmail.com>
2020-05-16 20:05:30 +0100

    Find pet by name

--- pet_shop.rb
+++ pet_shop.rb
@@ -31,3 +31,12 @@ def pets_by_breed(pet_shop, breed)
     }
     return pets
 end
+
+def find_pet_by_name(pet_shop, name)
+    pet_shop[:pets].each{ |pet| 
+        if (pet[:name] == name)
+            return pet
+        end
+    }
+    return nil
+end
